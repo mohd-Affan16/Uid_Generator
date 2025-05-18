5f66aa979d3844b8e3f091d16ad314312d65c5e9
mohd-Affan16 <mohdaffan0216@gmail.com>
2025-05-18 17:33:16 +0530

    Add UID generator script

---
+++ uid_generator.py
@@ -0,0 +1,4 @@
+import random
+character_list=['a','b','c','d','e','f','g','h','i','j','1','2','3','4','5','6','7','8','9']
+random_number=''.join(random.sample(character_list,10))
+print(random_number)
